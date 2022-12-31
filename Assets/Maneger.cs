7b0b338a9f4f892273ac2a9381eefb94cc5c33fb
Sulaksh Rajlawat <108143491+REDRIPPER1@users.noreply.github.com>
2023-01-01 00:24:31 +0545

    Happy New Year
    DARLING

--- Assets/Maneger.cs
+++ Assets/Maneger.cs
@@ -9,22 +9,33 @@ public class Maneger : MonoBehaviour
     public int current_dialogue = 0;
 
     public TextMeshProUGUI text_object;
+    public Sprite Bedroom_Pic;
+    public SpriteRenderer Background_object;
+        public Sprite waifupic;
+    public SpriteRenderer Waifu_object;
+
 
     void Start()
     {
         text_object.text= dialogues[current_dialogue];
     }
-
+  public int x;
     public void next_dialogue()
     {
         // INCREASEd CURR DIALOGUE BY 1
-        while(current_dialogue<10)
+       if(current_dialogue<10)
         {
-current_dialogue++;
-        }
+
+        
  text_object.text= dialogues[current_dialogue];
+ current_dialogue++;
+        }
+        if(current_dialogue>8)
+        {
+            Waifu_object.sprite=waifupic;
+            Background_object.sprite=Bedroom_Pic;
+        }
         // SET THE DIALOGUE TEXT.
     }
-
 }
 
